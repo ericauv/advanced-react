1e526d4508a2deaa3e1dcc815bed70267d65d0a7
ericauv <eriauv17@gmail.com>
2019-07-28 19:57:07 -0400

    Update Item

--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -13,6 +13,17 @@ const Mutations = {
     console.log(item);
 
     return item;
+  },
+  updateItem(parent, args, ctx, info) {
+    //first take a copy of updates
+    const updates = { ...args };
+    // Remove id from updates (since it cannot be updated)
+    delete updates.id;
+    // run update method
+    return ctx.db.mutation.updateItem(
+      { data: updates, where: { id: args.id } },
+      info
+    );
   }
 };
 
